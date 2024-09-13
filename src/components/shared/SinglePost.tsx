b75bbd45fa23c226b017c4380289254299a69df4
rubayetseason <rubayetislamseason5@gmail.com>
2024-09-13 13:22:06 +0600

    typography of tailwind added

--- src/components/shared/SinglePost.tsx
+++ src/components/shared/SinglePost.tsx
@@ -4,6 +4,24 @@ const SinglePost = () => {
   return (
     <div>
       <div className="max-w-[700px] mx-auto">
+        <div className="prose prose-red lg:prose-xl ">
+          <header>
+            <h1>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut?
+            </h1>
+            <p>
+              Published on August 5, 2024 by <strong>Admin</strong>
+            </p>
+          </header>
+        </div>
+        <div>
+          <img
+            src="https://picsum.photos/800/400"
+            className="rounded-md"
+            alt="blog_img"
+          />
+        </div>
+
         <div
           className="prose prose-red lg:prose-xl overflow-hidden"
           dangerouslySetInnerHTML={{ __html: data?.content }}
