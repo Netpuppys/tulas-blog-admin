ba5c5c5b4437058596e2e9bd7ed09d1094f6b421
Harsh Kumar rattewal <harshrattewal@gmail.com>
2025-01-18 12:00:17 +0530

    first commit

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
-      ? "https://blog.tis.edu.in/api/v1"
-      : "https://blog.tis.edu.in/api/v1",
+      ? "https://blog.tulas.edu.in/api/v1"
+      : "https://blog.tulas.edu.in/api/v1",
 });
 
 export default axiosInstance;
